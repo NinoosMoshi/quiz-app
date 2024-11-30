721c31c43a662c185e16ee3e71b40e1c8db99b92
NinoosMoshi <nalkaton@gmail.com>
2024-11-30 08:42:07 -0600

    backend create test

--- backEnd/quiz/src/main/java/com/ninos/controller/TestController.java
+++ backEnd/quiz/src/main/java/com/ninos/controller/TestController.java
@@ -1,15 +1,31 @@
 package com.ninos.controller;
 
+import com.ninos.dto.TestDTO;
+import com.ninos.entity.Test;
 import com.ninos.service.test.TestService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
-@RestController("/api/test")
+@RestController
+@RequestMapping("/api/test")
+@CrossOrigin("*")
 public class TestController {
 
     private final TestService testService;
 
+    @PostMapping
+    public ResponseEntity<?> createTest(@RequestBody TestDTO testDTO) {
+        try {
+            return new ResponseEntity<>(testService.createTest(testDTO), HttpStatus.CREATED);
+        }
+        catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+  
 
 
 }
