182bff7890170515e1e48c9ffbfdde5565b7d6a9
NinoosMoshi <nalkaton@gmail.com>
2024-12-13 10:18:40 -0600

    test question with test

--- backEnd/quiz/src/main/java/com/ninos/controller/TestController.java
+++ backEnd/quiz/src/main/java/com/ninos/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.ninos.controller;
 
+import com.ninos.dto.QuestionDTO;
 import com.ninos.dto.TestDTO;
 import com.ninos.entity.Test;
 import com.ninos.service.test.TestService;
@@ -20,6 +21,17 @@ public class TestController {
     public ResponseEntity<?> createTest(@RequestBody TestDTO testDTO) {
         try {
             return new ResponseEntity<>(testService.createTest(testDTO), HttpStatus.CREATED);
+        }
+        catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+    @PostMapping("/question")
+    public ResponseEntity<?> addQuestionInTest(@RequestBody QuestionDTO questionDTO) {
+        try {
+            return new ResponseEntity<>(testService.addQuestionInTest(questionDTO), HttpStatus.CREATED);
         }
         catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
